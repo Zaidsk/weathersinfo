df687b09b6eb44353a047b20750ce600a277797e
Zaid_SK <izaid37733@gmail.com>
2021-05-12 18:46:17 +0530

    Best UI for Heroku

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const constants = {
     openWeatherMap: {
         BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
-        SECRET_KEY: "3709f89e826c2838310e77a773533f2d"
+        SECRET_KEY: "d6322fb075233a996d7ef1e870c281e7"
     }
 }
 
