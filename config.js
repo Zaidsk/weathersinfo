3d15edd6b48572226099550e5e8b0262ed60af25
Zaidsk <63258233+Zaidsk@users.noreply.github.com>
2022-02-05 03:19:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const constants = {
     openWeatherMap: {
-        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        BASE_URL: "",
         SECRET_KEY: "d6322fb075233a996d7ef1e870c281e7"
     }
 }
